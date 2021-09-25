c7e639980af85aab35aff54f7014689355e7931d
imVipin <84066351+imVipin@users.noreply.github.com>
2021-09-25 12:51:36 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -22,7 +22,7 @@ class BotCommands:
 
 
 class Messages:
-    START_MSG = "**Hi there {}.**__\n\n__I'm Google Drive Uploader Bot.You can use me to upload any file / video to Google Drive from Direct Link or Telegram Files.__\n__\n__You can know more from /help.__ \n\n__┈┈┈••✿ @MyTestBotZ ✿••┈┈┈"
+    START_MSG = "**Hi there {}.**__\n\n__**I'm Google Drive Uploader Bot.**\n\n<code>You can use me to upload any file / video to Google Drive from Direct Link or Telegram Files.</code>__\n\n **🚨Note: I will not listen to any command or message (except /auth command) until you authorize me.\nSo, Authorization is mandatory !** __\n__You can know more from /help.__ \n\n__┈┈┈••✿ @MyTestBotZ ✿••┈┈┈"
 
     HELP_MSG = [
         ".",
